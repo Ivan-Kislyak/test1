cd277552cc4e2411491319cd5920b859270e1cd1
Ivan-Kislyak <ivangrodno86@gmail.com>
2022-11-13 18:20:27 +0200

    Init

--- src/test1/HelloWorld.java
+++ src/test1/HelloWorld.java
@@ -5,5 +5,12 @@ public class HelloWorld {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
