7b1b8db10f596a277b91590a41cada8ceed63b7f
Ivan-Kislyak <ivangrodno86@gmail.com>
2022-11-21 21:38:05 +0200

    Init commit

--- src/test1/HelloWorld.java
+++ src/test1/HelloWorld.java
@@ -2,10 +2,7 @@ package test1;
 
 public class HelloWorld {
     public static  void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Check1");
-        System.out.println("Check2");
-        System.out.println("Check3");
-
+        System.out.println("Check connection");
+        System.out.println("Next check");
     }
 }
